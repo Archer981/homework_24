62bbd01aa0fc1c64dd34f42ae64cf8b936d82823
buzov <abuzov@list.ru>
2023-04-20 20:45:43 +0300

    Completed

--- app.py
+++ app.py
@@ -1,27 +1,30 @@
 import os
 
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, abort, jsonify, Response
+from typing import Callable, Dict, Any, Optional, Union
 
-from utils import filter_query, map_query, unique_query, sort_query, limit_query
+from utils import filter_query, map_query, unique_query, sort_query, limit_query, regex_query
 
 app = Flask(__name__)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
-query_dict = {'filter': filter_query,
+query_dict: dict[str, Callable] = {'filter': filter_query,
               'map': map_query,
               'unique': unique_query,
               'sort': sort_query,
-              'limit': limit_query}
+              'limit': limit_query,
+              'regex': regex_query,
+                                   }
 
 
 @app.route("/perform_query", methods=['POST'])
-def perform_query():
-    query = request.json
+def perform_query() -> Response:
+    query: dict = request.json
     if not os.path.exists(os.path.join(DATA_DIR, query['file_name'])):
         abort(400)
     with open(os.path.join(DATA_DIR, query['file_name']), 'r', encoding='utf-8') as file:
-        data = file.readlines()
+        data: list[str] = file.readlines()
     for i in range(len(query) // 2):
         data = query_dict[query['cmd' + str(i + 1)]](data, query['value' + str(i + 1)])
     return jsonify(data)
--- utils.py
+++ utils.py
@@ -1,19 +1,28 @@
-def filter_query(data, query):
+import re
+from typing import Iterator, Optional
+
+
+def filter_query(data: list[str], query: str) -> list:
     return [line for line in data if query.lower() in line.lower()]
 
 
-def map_query(data, query):
+def map_query(data: list[str], query: str) -> list:
     return list(map(lambda x: x.split(' ')[int(query)], data))
 
 
-def unique_query(data, query=''):
+def unique_query(data: list[str], query: None = None) -> list:
     return list(set(data))
 
 
-def sort_query(data, query='asc'):
-    query = False if query == 'asc' else True
-    return sorted([line for line in data], reverse=query)
+def sort_query(data: list[str], query: str = 'asc') -> list:
+    flag: bool = False if query == 'asc' else True
+    return sorted([line for line in data], reverse=flag)
 
 
-def limit_query(data, query):
+def limit_query(data: list[str], query: str) -> list:
     return [line for line in data][:int(query)]
+
+
+def regex_query(data: list[str], query: str) -> Iterator:
+    pattern: re.Pattern = re.compile(query)
+    return filter(lambda x: re.search(pattern, x), data)
